2c9ec33426dba95c2b786ea92858899783bc26a5
hao <unnamed.hao@gmail.com>
2021-01-20 19:02:27 +0800

    add

--- base.go
+++ base.go
@@ -16,7 +16,7 @@ func getK(old string) string {
 	old = strings.ReplaceAll(old, "`", "_")
 	var buf bytes.Buffer
 	buf.WriteString(old)
-	buf.WriteString("_")
+	buf.WriteString("_+")
 	buf.WriteString(strconv.FormatInt(globalIndex, 10))
 	return buf.String()
 }
@@ -55,6 +55,36 @@ func (o ConvertEq) AppendToQuery(buf bytes.Buffer, arg map[string]interface{}) (
 		return bytes.Buffer{}, nil, err
 	}
 	_, err = buf.WriteString("=:")
+	if err != nil {
+		return bytes.Buffer{}, nil, err
+	}
+	_, err = buf.WriteString(k)
+	if err != nil {
+		return bytes.Buffer{}, nil, err
+	}
+	arg[k] = o.V
+	return buf, arg, nil
+}
+
+// ConvertAdd k=k+:k
+type ConvertAdd ConvertKv
+
+func (o ConvertAdd) AppendToQuery(buf bytes.Buffer, arg map[string]interface{}) (bytes.Buffer, map[string]interface{}, error) {
+	k := getK(o.K)
+
+	_, err := buf.WriteString(o.K)
+	if err != nil {
+		return bytes.Buffer{}, nil, err
+	}
+	_, err = buf.WriteString("=")
+	if err != nil {
+		return bytes.Buffer{}, nil, err
+	}
+	_, err = buf.WriteString(o.K)
+	if err != nil {
+		return bytes.Buffer{}, nil, err
+	}
+	_, err = buf.WriteString("+:")
 	if err != nil {
 		return bytes.Buffer{}, nil, err
 	}
