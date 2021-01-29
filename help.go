2d268375f051bed1dca01b6f6fb748691402c57c
hao <unnamed.hao@gmail.com>
2021-01-29 13:47:30 +0800

    add help func

---
+++ help.go
@@ -0,0 +1,58 @@
+package mquery
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/moremorefun/mcommon"
+)
+
+// DbSelectMapOne2One 获取关联map
+func DbSelectMapOne2One(ctx context.Context, tx mcommon.DbExeAble, sourceRows []map[string]interface{}, sourceKey, targetTableName, targetKey string, targetColumns []string) (map[string]interface{}, error) {
+	var keyValues []interface{}
+	for _, sourceRow := range sourceRows {
+		keyValues = append(keyValues, sourceRow[sourceKey])
+	}
+	targetRows, err := Select().
+		ColumnsString(targetColumns...).
+		FromString(targetTableName).
+		Where(ConvertEqMake(targetKey, keyValues)).
+		RowsInterface(
+			ctx,
+			tx,
+		)
+	if err != nil {
+		return nil, err
+	}
+	targetMap := map[string]interface{}{}
+	for _, targetRow := range targetRows {
+		k := fmt.Sprintf("%v", targetRow[targetKey])
+		targetMap[k] = targetRow
+	}
+	return targetMap, nil
+}
+
+// DbSelectMapOne2Many 获取关联map
+func DbSelectMapOne2Many(ctx context.Context, tx mcommon.DbExeAble, sourceRows []map[string]interface{}, sourceKey, targetTableName, targetKey string, targetColumns []string) (map[string][]interface{}, error) {
+	var keyValues []interface{}
+	for _, sourceRow := range sourceRows {
+		keyValues = append(keyValues, sourceRow[sourceKey])
+	}
+	targetRows, err := Select().
+		ColumnsString(targetColumns...).
+		FromString(targetTableName).
+		Where(ConvertEqMake(targetKey, keyValues)).
+		RowsInterface(
+			ctx,
+			tx,
+		)
+	if err != nil {
+		return nil, err
+	}
+	targetMap := map[string][]interface{}{}
+	for _, targetRow := range targetRows {
+		k := fmt.Sprintf("%v", targetRow[targetKey])
+		targetMap[k] = append(targetMap[k], targetRow)
+	}
+	return targetMap, nil
+}
